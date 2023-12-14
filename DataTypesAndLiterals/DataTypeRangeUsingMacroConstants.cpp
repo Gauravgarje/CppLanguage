d7c15b06f8fc391b50cfd4dbc22a2d870bbed57e
Gauravgarje <garjegaurav2000@gmail.com>
2023-12-14 15:02:51 +0530

    Data Types Commit Part One

---
+++ DataTypesAndLiterals/DataTypeRangeUsingMacroConstants.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+int main()
+{
+    cout << "Size of char: " << sizeof(char) << " byte" << endl;
+    cout << "Char minimum value: " << CHAR_MIN << endl;
+    cout << "Char maximum value: " << CHAR_MAX << endl;
+    return 0;
+}
