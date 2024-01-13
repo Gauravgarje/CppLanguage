4efb85a4c1c33d72e895df2bdb35f75ceffad75e
Gauravgarje <garjegaurav2000@gmail.com>
2024-01-13 10:08:32 +0530

    added other methods to implement constants

--- VariablesAndConstants/ConstantVariableImplementation.cpp
+++ VariablesAndConstants/ConstantVariableImplementation.cpp
@@ -6,13 +6,24 @@ using namespace std;
 
 int main()
 {
+    // Method 1: Using const keyword
+    // Syntax: const DATATYPE variable_name = value;
     const int int_const = 4;
     const char char_const = 'A';
     const float float_const = 54.54;
 
-    cout << int_const << endl;
-    cout << char_const << endl;
-    cout << float_const << endl;
+    cout << int_const << endl;   // Output: 4
+    cout << char_const << endl;  // Output: A
+    cout << float_const << endl; // Output: 54.54
 
-    cout << PI << endl;
+    // Method 2: Using constexpr keyword
+    // Syntax: constexpr DATATYPE variable_name = value;
+    constexpr int hours_in_day = 24;
+
+    cout << hours_in_day << endl; // Output: 24
+
+    // Method 2: Using constexpr keyword
+    // Syntax: #define MACRO_NAME replacement_value
+
+    cout << PI << endl; // Output: 3.14
 }
